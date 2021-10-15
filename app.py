08d1c14f23221b32a634037708a93c0d084aec48
ngocuong <ngo.cuong@abv.bg>
2021-10-15 13:11:21 +0300

    refactor

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pages
 about_info = 'Investing real money using this app is highly discouraged.'
 menu_items = {'About': about_info}
 
-st.set_page_config(page_title='Apothemata',\
+st.set_page_config(page_title='',\
                     page_icon='🅰️',\
                     layout='wide',\
                     initial_sidebar_state='auto',\
@@ -14,12 +14,14 @@ st.set_page_config(page_title='Apothemata',\
 # Create an instance of the app 
 app = MultiPage()
 home = pages.home(title = 'Home')
-checkStock = pages.checkStock(title = 'Check your stocks')
-trade = pages.trade(title = 'Yolo trade')
+checkStock = pages.checkStock(title = 'Get Stock Prices')
+yoloData = pages.getYoloData(title = 'Get Yolo Data')
+trade = pages.trade(title = 'Yolo Trade')
 
 # Add all your application here
 app.add_page(home)
 app.add_page(checkStock)
+app.add_page(yoloData)
 app.add_page(trade)
 
 # The main app
